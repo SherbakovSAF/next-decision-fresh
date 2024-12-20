b692487b6d000417bfc8ebadb64479b17a3cceb1
Sergei <shcherbakov.s.a@steos.io>
2024-12-21 01:06:39 +0300

    авыаы

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
