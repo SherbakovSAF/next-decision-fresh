604c4540cee0b7df19288b4b0a0dbc536fc7763a
Sergei <shcherbakov.s.a@steos.io>
2024-12-21 00:17:01 +0300

    1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,11 @@
 import { User } from "@prisma/client";
-import prisma from "../../lib/prisma";
+import prisma from "@/../prisma/prisma.client";
 
 export default async function Home() {
   const users: User[] = await prisma.user.findMany();
 
-  // const createUser = async () => {
-  //   const newUser = await prisma.user.create({
-  //     data: {
-  //       email: email,
-  //     },
-  //   });
-  //   users.push(newUser);
-  // };
-
-  // const [email, setEmail] = useState("");
-  // const [name, setName] = useState("");
-
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16">
-      {/* <input
-        placeholder="email"
-        value={email}
-        onChange={(event) => setEmail(event.target.value)}
-      />
-      <input
-        placeholder="name"
-        value={name}
-        onChange={(event) => setName(event.target.value)}
-      /> */}
       {users.map((userF) => {
         return (
           <div key={userF.id}>
@@ -37,7 +15,6 @@ export default async function Home() {
           </div>
         );
       })}
-      {/* <button onClick={() => createUser()}>Создать</button> */}
     </div>
   );
 }
