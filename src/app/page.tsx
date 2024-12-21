28bea8b90b9d1115a0b4d182c1d5ea88a7e08daf
Sergei <shcherbakov.s.a@steos.io>
2024-12-21 23:26:59 +0300

    123

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,9 @@ import { User } from "@prisma/client";
 import prisma from "@/../prisma/prisma.client";
 
 export default async function Home() {
-  const users: User[] = await prisma.user.findMany();
+  const result = await prisma.user.findMany();
+
+  const users: User[] = result ?? [];
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16">
