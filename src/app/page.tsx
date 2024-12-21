572ca088c8e5d161334ed7c211c6749ddef89930
Sergei <shcherbakov.s.a@steos.io>
2024-12-21 23:30:12 +0300

    432432

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { User } from "@prisma/client";
 import prisma from "@/../prisma/prisma.client";
+import { Suspense } from "react";
 
 export default async function Home() {
   const result = await prisma.user.findMany();
@@ -7,16 +8,18 @@ export default async function Home() {
   const users: User[] = result ?? [];
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16">
-      {users.map((userF) => {
-        return (
-          <div key={userF.id}>
-            {userF.id}
-            {userF.name}
-            {userF.email}
-          </div>
-        );
-      })}
-    </div>
+    <Suspense fallback={<div>Load</div>}>
+      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16">
+        {users.map((userF) => {
+          return (
+            <div key={userF.id}>
+              {userF.id}
+              {userF.name}
+              {userF.email}
+            </div>
+          );
+        })}
+      </div>
+    </Suspense>
   );
 }
